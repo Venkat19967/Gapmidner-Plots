6e519520e877d7e7a603d187b30b4a20a23e3676
Venkatramanan Srinivasan <venkyman@Venkat.local>
2020-10-17 18:36:18 -0700

    Basic scatterplot

--- scripts/homework4.js
+++ scripts/homework4.js
@@ -1,5 +1,5 @@
 var max_income;
-var max_life_expectancy;
+var max_life;
 var max_population;
 var max_tsunami;
 var max_children;
@@ -7,8 +7,17 @@ var max_children;
 var regions;
 var regionset;
 
+var xattr;
+var yattr;
+var reg;
+var yrinput;
+
 var jsonVal;
 var jsonVal_max;
+var svg;
+
+var currentx;
+var currenty;
 
 
 // This runs when the page is loaded
@@ -18,8 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
   
     regionset = new Set();
     div = d3.select("body").append("div")
-  
-   
+
   
     // Load both files before doing anything else
     Promise.all([d3.csv('data/countries_regions.csv'),
@@ -33,13 +41,13 @@ document.addEventListener('DOMContentLoaded', function() {
         jsonVal_max = values[2];
         
         max_income = parseInt(jsonVal_max.max_income);
-        max_life_expectancy = parseFloat(jsonVal_max.max_life);
+        max_life = parseFloat(jsonVal_max.max_life);
         max_population = parseInt(jsonVal_max.max_population);
         max_children = parseFloat(jsonVal_max.max_children);
         max_tsunami = parseInt(jsonVal_max.max_tsunami);
 
         console.log(max_income);
-        console.log(max_life_expectancy);
+        console.log(max_life);
         console.log(max_population);
         console.log(max_children);
         console.log(max_tsunami);
@@ -61,39 +69,80 @@ document.addEventListener('DOMContentLoaded', function() {
             r.add(x);
         })
   
-      
+        // scatterplot();
+        getfields()
     })
-
-    scatterplot();
   
 });
 
+
+
+function getfields() {
+
+    xattr = document.getElementById("x-attribute").value;
+    yattr = document.getElementById("y-attribute").value;
+    yrinput = document.getElementById("year-input").value;
+    yrinput = +yrinput;
+    reg = document.getElementById("region").value;
+    scatterplot();
+
+    
+}
+
+
+function plotaxes(){
+
+}
+
+
 function scatterplot(){
     
-    var x = d3.scaleLinear().domain([0, max_population]);
-    var y = d3.scaleLinear().domain([0, max_income]);
+    var x = d3.scaleLinear().domain([0, eval("max_" + xattr)]).range([ 0, 900 ]);
+    var y = d3.scaleLinear().domain([0, eval("max_" + yattr)]).range([ 600, 0 ]);
+    
 
-    svg.append('g')
-    .selectAll("dot")
+    
+    svg.append("g")
+    .attr("transform", "translate("+ 75+ "," + 650 + ")")
+    .attr('id', 'x-axis')
+    .call(d3.axisBottom(x));
+
+    svg.append("g")
+    .attr('id', 'y-axis')
+    .attr("transform", "translate("+ 75+ "," + 50 + ")")
+    .call(d3.axisLeft(y));
+
+    
+    
+    svg.selectAll("dot")
     .data(jsonVal)
-    // .enter()
-    // .append("circle")
-    .attr("cx", function (d) { 
-          temp = []
-          d.forEach(d =>{
-              temp.append(d.population[200])
-          });
-          console.log(temp);
-          return temp; 
-        })
-      .attr("cy", function (d) {
-        temp = []
-          d.forEach(d =>{
-              temp.append(d.income[200])
-          })
-          return temp;
-      })
-      .attr("r", 1.5)
-      .style("fill", "#69b3a2")
+    // .filter(function(d) { if(d.region == reg){return d;}})
+    .enter().append("circle")
+    .attr("fill", "white")
+    .attr("stroke","black")
+    .attr("fill-opacity","0")
+    .attr("r", 20)
+    .attr("transform", "translate("+ 75+ "," + 50 + ")")
+    .attr("cx", 
+    function(d) { 
+        if(eval("d."+ xattr + "[yrinput-1800]") == 'NaN'){
+            return x(0);
+        }
+        else{
+            // console.log(eval(`d.tsunami[yrinput-1800]`));
+            return x(parseInt(eval("d."+ xattr + "[yrinput-1800]")));
+        }
+    })
+    .attr("cy", function(d) { 
+        // if(d.population[yrinput-1800])
+        if(eval("d."+ yattr + "[yrinput-1800]") == 'NaN'){
+            return y(0);
+        }
+        else{
+            return y(parseInt(eval("d."+ yattr + "[yrinput-1800]")));
+        }
+    });
+
+
 
 }
